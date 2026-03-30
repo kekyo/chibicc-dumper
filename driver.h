28857b7afd93cb10a8f2994bc193a9444a37dabe
Kouji Matsui <k@kekyo.net>
2026-03-30 18:27:36 +0900

    refactor: split CLI driver from main entrypoint

---
+++ driver.h
@@ -0,0 +1,12 @@
+#ifndef CHIBICC_DRIVER_H
+#define CHIBICC_DRIVER_H
+
+/**
+ * @brief Runs the native chibicc-dumper CLI driver.
+ * @param argc Number of command-line arguments.
+ * @param argv Command-line argument vector.
+ * @return Process exit status.
+ */
+int chibicc_driver_main(int argc, char **argv);
+
+#endif
